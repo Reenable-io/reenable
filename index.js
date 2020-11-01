6501552b02efd05bd01fcd7945e2d2b09f103241
Nicolas Buchwalder <nicolas.buchwalder@epfl.ch>
2020-11-01 18:07:30 +0100

    superette

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var rep = prompt("Chaud pour une partie de bilboquet?")
-switch(rep){
-    case "oui":
-        alert("super, petit sexe")
-        break;
-    case "non":
-        alert("je t'encule stp")
-        break;
-    default:
-        alert("parle francais stp")
-        break;
+while (true){
+    var rep = prompt("Chaud pour une partie de bilboquet?")
+    switch(rep){
+        case "oui":
+            alert("super, petit sexe")
+            break;
+        case "non":
+            alert("je t'encule stp")
+            break;
+        default:
+            alert("parle francais stp")
+            break;
+    }
 }
