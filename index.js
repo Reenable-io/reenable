ffe0d85b6cb383ca63fc1448e66d0e542ecbe072
Nicolas Buchwalder <nicolas.buchwalder@epfl.ch>
2020-11-01 18:31:42 +0100

    superettoyeeeee

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-if (confirm("Est-ce que tu veux une info intéressante?")) {
+if (confirm("Est-ce que tu veux une info interessante?")) {
     var today = new Date();
-    diff = today.getFullYear() - 2006;
-    alert('Cela fait plus ou moins' + diff + 'ans que notre fdp de CTO est né.'  );
+    years = today.getFullYear() - 2006;
+    months = today.getMonth() - 6;
+    days = today.getDay() - 16;
+    if (days<0){
+        days = days + 30;
+        months = months -1
+    }
+    hours = today.getHours()-8;
+    if (hours<0){
+        hours = hours + 24;
+        days = days -1
+    }
+    minutes = today.getMinutes()-17;
+    if (minutes<0){
+        minutes = minutes + 60;
+        hours = hours -1
+    }
+    seconds = today.getSeconds();
+    alert('Cela fait plus ou moins ' + years + ' ans, ' + months + ' mois, ' + days + ' jours,' + hours + ' heures, ' + minutes + ' minutes et ' + seconds + ' secondes que je dois me coltiner ce ptn de CTO qui me sert de frere. '  );
     confirm("Intéressant nan?")
 }
 else {
