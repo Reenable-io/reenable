94c5385e88939a9f93faa52ca55315c1d8e95de1
flexflower <kelyancsajka@icloud.com>
2020-11-02 00:13:51 +0100

    m?

--- ahaha.js
+++ ahaha.js
@@ -1,8 +1,1 @@
-identite = prompt("t'es nico?", "oui / non")
-if (identite = "oui") {
-    var newDiv = document.createElement('div')
-    var textHere = document.createTextNode('ok')
-    newDiv.appendChild(textHere)
-    var addHere = document.getElementById('addHere')
-    addHere.appendChild(newDiv)
-}
+
