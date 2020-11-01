2c88089f13fc28a0c937105b6f32b01e0df5c229
Nicolas Buchwalder <nicolas.buchwalder@epfl.ch>
2020-11-01 18:03:27 +0100

    super

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert('LA BITOQ')
+var rep = prompt("Chaud pour une partie de bilboquet?")
+switch(rep){
+    case "oui":
+        alert("super, petit sexe")
+        break;
+    case "non":
+        alert("je t'encule stp")
+        break;
+    default:
+        alert("parle francais stp")
+        break;
+}
