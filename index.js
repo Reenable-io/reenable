aff501808be86fca1deda703b2d0a453662c7a22
Nicolas Buchwalder <nicolas.buchwalder@epfl.ch>
2020-11-01 18:20:34 +0100

    superettoye

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-while (true){
-    var rep = prompt("Chaud pour une partie de bilboquet?")
-    switch(rep){
-        case "oui":
-            alert("super, petit sexe")
-            break;
-        case "non":
-            alert("je t'encule stp")
-            break;
-        default:
-            alert("parle francais stp")
-            break;
-    }
+if (confirm("Est-ce que tu veux une info intéressante?")) {
+    var today = new Date();
+    diff = today.getFullYear() - 2006;
+    alert('Cela fait plus ou moins' + diff + 'ans que notre fdp de CTO est né.'  );
+    confirm("Intéressant nan?")
+}
+else {
+    alert("Dommage...")
 }
