f73cd08f4231ce8f09123a0ba46b69b7c3464d93
flexflower <kelyancsajka@icloud.com>
2020-11-01 18:37:28 +0100

    format

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ if (confirm("Est-ce que tu veux une info interessante?")) {
     years = today.getFullYear() - 2006;
     months = today.getMonth() - 6;
     days = today.getDay() - 16;
-    if (days<0){
+    if (days < 0) {
         days = days + 30;
-        months = months -1
+        months = months - 1
     }
-    hours = today.getHours()-8;
-    if (hours<0){
+    hours = today.getHours() - 8;
+    if (hours < 0) {
         hours = hours + 24;
-        days = days -1
+        days = days - 1
     }
-    minutes = today.getMinutes()-17;
-    if (minutes<0){
+    minutes = today.getMinutes() - 17;
+    if (minutes < 0) {
         minutes = minutes + 60;
-        hours = hours -1
+        hours = hours - 1
     }
     seconds = today.getSeconds();
-    alert('Cela fait plus ou moins ' + years + ' ans, ' + months + ' mois, ' + days + ' jours,' + hours + ' heures, ' + minutes + ' minutes et ' + seconds + ' secondes que je dois me coltiner ce ptn de CTO qui me sert de frere. '  );
+    alert('Cela fait plus ou moins ' + years + ' ans, ' + months + ' mois, ' + days + ' jours, ' + hours + ' heures, ' + minutes + ' minutes et ' + seconds + ' secondes que je dois me coltiner ce ptn de CTO qui me sert de frere. ');
     confirm("Intéressant nan?")
 }
 else {
